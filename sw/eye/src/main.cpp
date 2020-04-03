996c8c21e33309a3f327c3ec9ee30b0d6ee2aa23
an-dr <mail@agramakov.me>
2020-04-03 20:40:26 +0100

    implemented i2c with registers

--- sw/eye/src/main.cpp
+++ sw/eye/src/main.cpp
@@ -1,32 +1,37 @@
+#include "I2c.h"
+#include "VirtualRegs.h"
+#include "printf.h"
 #include <Arduino.h>
 #include <Wire.h>
 #define BUILTIN_LED 13
+#define I2C_ADDRESS 0x2b
+#define REGS_NUM 8
+
 int sensorPin = A3;  // select the input pin
 int sensorValue = 0; // variable to store the value coming from the sensor
 
-
-void requestEvent() {
-    Wire.write(sensorValue);
-    Wire.write(sensorValue>>8);
-                                 // as expected by master
-}
+I2C i2c;
+void i2c_reqEv() { I2C::requestEvent(i2c); }
+void i2c_rcvEv(int howMany) { I2C::receiveEvent(i2c); }
 
 void setup() {
-
-    Wire.begin(0x2b);             // join i2c bus with address #43
-    Wire.onRequest(requestEvent); // register event
+    i2c.Init(I2C_ADDRESS, 8, (void *)i2c_reqEv, (void *)i2c_rcvEv);
     pinMode(BUILTIN_LED, OUTPUT); // Initialize the BUILTIN_LED pin as an output
-    pinMode(A2, OUTPUT); // Initialize the BUILTIN_LED pin as an output
-    digitalWrite(A2, HIGH); // Initialize the BUILTIN_LED pin as an output
+    pinMode(A2, OUTPUT);          // Initialize the BUILTIN_LED pin as an output
+    digitalWrite(A2, HIGH);       // Initialize the BUILTIN_LED pin as an output
     Serial.begin(9600);
+    printf_init();
 }
 
 void loop() {
-    sensorValue = analogRead(sensorPin);
-    Serial.println(sensorValue, HEX); //debug
-    if (sensorValue <= 120) {
+    int val = analogRead(sensorPin);
+    i2c.Set(1,(uint8_t)(val>>0)&0xFF);
+    i2c.Set(0,(uint8_t)(val>>8)&0xFF);
+    // i2c.SelectReg(0);
+    // i2c.SetRegAndNext((uint8_t)(val>>0)&0xFF);
+    // i2c.SetRegAndNext((uint8_t)(val>>8)&0xFF);
+    if (val <= 120) {
         digitalWrite(BUILTIN_LED, HIGH); //led off
-        delay(1000);
     }
-    digitalWrite(BUILTIN_LED, LOW); //led on - default
+    digitalWrite(BUILTIN_LED, LOW); //led on - defaults
 }
